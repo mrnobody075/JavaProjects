fa55ba06e0532b1a8b90784fffc758257d050038
mrnobody075 <hegdevishals75@gmail.com>
2024-08-03 22:43:05 +0530

    yesdoit

---
+++ exp.java
@@ -0,0 +1,12 @@
+class exp {
+	public static void main(String[] args) {
+		// Implement your code here 
+		double pi = 3.14;
+		double r1 = 50.24;
+		double r2 = 314.0;
+		double a1=pi*r1*r1;
+		double a2=pi*r2*r2;
+		System.out.print(a1,a2);
+	}
+}
+
---
+++ test.java
@@ -0,0 +1,6 @@
+class test {
+	public static void main(String[] args) {
+		// Implement your code here 
+		System.out.print("Door No: D089\nStreet: St. Louis Street\nCity: Springfield\nZIP Code: 62729");
+	}
+}
