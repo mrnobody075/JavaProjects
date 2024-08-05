9347cf1f5e2649e6789a5f7ebc87309b7664ac39
mrnobody075 <hegdevishals75@gmail.com>
2024-08-05 20:13:55 +0530

    i

--- carLoan.java
+++ carLoan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CarLoan {
+public class carLoan {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
